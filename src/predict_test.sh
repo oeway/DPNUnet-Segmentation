3b2508ea873d170a03d9ea183087c3a19b2edd1f
oeway <oeway007@gmail.com>
2019-05-21 01:29:01 +0200

    add s2 file

--- src/predict_test.sh
+++ src/predict_test.sh
@@ -1,3 +1,3 @@
-python bowl_eval.py ./configs/dpn_softmax_s2.json
+CUDA_VISIBLE_DEVICES=3 python bowl_eval.py ./configs/imjoy_dpn_softmax.json
 #python bowl_eval.py ./configs/dpn_sigmoid_s2.json
 #python bowl_eval.py ./configs/resnet_softmax_s2.json
