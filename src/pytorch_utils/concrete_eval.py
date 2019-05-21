f7df35cb0235a254197b8404c9e16a4636c5b12f
oeway <oeway007@gmail.com>
2019-05-21 19:07:06 +0200

    add save hook to prediction

--- src/pytorch_utils/concrete_eval.py
+++ src/pytorch_utils/concrete_eval.py
@@ -11,6 +11,10 @@ from .eval import Evaluator
 class FullImageEvaluator(Evaluator):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+        if 'step_callback' in kwargs:
+            self.step_callback = kwargs['step_callback']
+        else:
+            self.step_callback = None
 
     def process_batch(self, predicted, model, data, prefix=""):
         names = data['image_name']
@@ -31,9 +35,18 @@ class FullImageEvaluator(Evaluator):
             prediction = cv2.cvtColor(prediction, cv2.COLOR_RGB2BGR)
         if self.test:
             name = os.path.split(name)[-1]
+        
         save_path = os.path.join(self.config.dataset_path, self.ds.fn_mapping['masks'](name)) # os.path.join(self.save_dir + prefix, self.ds.fn_mapping['masks'](name))
-        print('saving to ',  save_path)
-        folder, name = os.path.split(save_path)
-        if not os.path.exists(folder):
-            os.makedirs(folder)
-        cv2.imwrite(save_path, (prediction * [255,255,0]).astype(np.uint8))
+
+        do_save = True
+        if self.step_callback is not None:
+            skip_save = self.step_callback({'prediction': prediction, 'save_path': save_path})
+            if skip_save:
+                do_save = False
+
+        if do_save:
+            print('saving to ',  save_path)
+            folder, name = os.path.split(save_path)
+            if not os.path.exists(folder):
+                os.makedirs(folder)
+            cv2.imwrite(save_path, (prediction * [255,255,0]).astype(np.uint8))
