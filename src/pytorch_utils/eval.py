471caa5521e4695ef5445aee498654c84a5225ca
oeway <oeway007@gmail.com>
2019-05-22 14:02:15 +0200

    move predict_batch

--- src/pytorch_utils/eval.py
+++ src/pytorch_utils/eval.py
@@ -89,6 +89,24 @@ def read_model(model_path):
     model.eval()
     return model
 
+def predict_batch(model, batch, sigmoid=False, cropping=None):
+    predicted_batch = predict8tta(model, batch, sigmoid)
+    results = []
+    if cropping is not None:
+        rows, cols = cropping
+    else:
+        orig_shape = predicted_batch[0,...].shape
+        rows, cols = orig_shape[:2]
+    for b in range(predicted_batch):
+        prediction = predicted_batch[i,...]
+        prediction = prediction[0:rows, 0:cols,...]
+        if prediction.shape[2] < 3:
+            zeros = np.zeros((rows, cols), dtype=np.float32)
+            prediction = np.dstack((prediction[...,0], prediction[...,1], zeros))
+        else:
+            prediction = cv2.cvtColor(prediction, cv2.COLOR_RGB2BGR)
+        results.append(prediction)
+    return results
 
 class Evaluator:
     def __init__(self, config, ds, test=False, flips=0, num_workers=0, border=12, val_transforms=None):
@@ -108,31 +126,12 @@ class Evaluator:
         self.val_transforms = val_transforms
         os.makedirs(self.save_dir, exist_ok=True)
 
-    def predict_batch(model, batch, cropping=None):
-        predicted_batch = predict8tta(model, batch, self.config.sigmoid)
-        results = []
-        if cropping is not None:
-            rows, cols = cropping
-        else:
-            orig_shape = predicted_batch[0,...].shape
-            rows, cols = orig_shape[:2]
-        for b in range(predicted_batch):
-            prediction = predicted_batch[i,...]
-            prediction = prediction[0:rows, 0:cols,...]
-            if prediction.shape[2] < 3:
-                zeros = np.zeros((rows, cols), dtype=np.float32)
-                prediction = np.dstack((prediction[...,0], prediction[...,1], zeros))
-            else:
-                prediction = cv2.cvtColor(prediction, cv2.COLOR_RGB2BGR)
-            results.append(prediction)
-        return results
-        
-
-    def predict(self, model_path, val_indexes, prefix):
+    def predict(self, model, val_indexes, prefix):
         val_dataset = SequentialDataset(self.ds, val_indexes, stage='test', config=self.config, transforms=self.val_transforms)
         val_dl = PytorchDataLoader(val_dataset, batch_size=self.config.predict_batch_size, num_workers=self.num_workers, drop_last=False)
 
-        model = read_model(model_path)
+        if type(model) is str:
+            model = read_model(model)
 
         pbar = tqdm.tqdm(val_dl, total=len(val_dl))
         for data in pbar:
